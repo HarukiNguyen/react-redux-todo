7515dee6706441e39a3f45d3abebcfa7f4b2ebaf
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-10-29 12:23:45 +0700

    refactor: :recycle: remove postData middewares

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todosReducer from "../features/TodosList/todosSlice";
 import filtersReducer from "../features/Filters/filtersSlice";
-import { postData } from "../enhancers/dataPersistance";
 
 export const store = configureStore({
   reducer: {
     todos: todosReducer,
     filters: filtersReducer,
   },
-  middleware: [postData],
 });
