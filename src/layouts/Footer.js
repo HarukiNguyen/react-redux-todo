0741049c3d0eab8bae162fcf9df8bce71ab23027
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-10-29 11:15:40 +0700

    feat: :sparkles: add clear all completed feature

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,12 +1,19 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Filters from "../features/Filters/Filters";
-import { selectAllTodos } from "../features/TodosList/todosSlice";
+import {
+  selectAllTodos,
+  clearCompleted,
+} from "../features/TodosList/todosSlice";
 
 function Footer() {
   const todos = useSelector(selectAllTodos);
   const todoLength = todos.length;
-
+  const dispatch = useDispatch();
   const suffix = todoLength > 1 ? "s" : "";
+
+  function handleClearCompleted() {
+    dispatch(clearCompleted());
+  }
 
   return (
     <footer className="footer">
@@ -14,7 +21,9 @@ function Footer() {
         <strong>{todoLength}</strong> item{suffix} left
       </span>
       <Filters />
-      <button className="clear-completed">Clear completed</button>
+      <button className="clear-completed" onClick={handleClearCompleted}>
+        Clear completed
+      </button>
     </footer>
   );
 }
