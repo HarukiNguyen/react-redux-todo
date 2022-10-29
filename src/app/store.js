ad9d5859d9f8b82f7a2ed0c081ef582d42f9b543
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-10-29 12:16:28 +0700

    feat: :sparkles: setup store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todosReducer from "../features/TodosList/todosSlice";
+import filtersReducer from "../features/Filters/filtersSlice";
+import { postData } from "../enhancers/dataPersistance";
 
 export const store = configureStore({
   reducer: {
-    todos: todosReducer
+    todos: todosReducer,
+    filters: filtersReducer,
   },
+  middleware: [postData],
 });
