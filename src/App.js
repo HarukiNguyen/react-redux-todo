291568921a5a77a433899f94ee8aa79f389e28e8
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-10-29 08:53:01 +0700

    feat: :sparkles: split layout, features; hide content and footer when there's no todo

--- src/App.js
+++ src/App.js
@@ -1,61 +1,27 @@
+import { useSelector } from "react-redux";
+import { selectAllTodos } from "./features/TodosList/todosSlice";
+import Content from "./layouts/Content";
+import Footer from "./layouts/Footer";
+import Header from "./layouts/Header";
+
 function App() {
+  const todos = useSelector(selectAllTodos);
+  const todoLength = todos.length;
+
   return (
     <>
       <section className="todoapp">
-        <header className="header">
-          <h1>todos</h1>
-          <input
-            className="new-todo"
-            placeholder="What needs to be done?"
-            autoFocus
-          />
-        </header>
-        <section className="main">
-          <input id="toggle-all" className="toggle-all" type="checkbox" />
-          <label htmlFor="toggle-all">Mark all as complete</label>
-          <ul className="todo-list">
-            <li className="completed">
-              <div className="view">
-                <input className="toggle" type="checkbox" defaultChecked />
-                <label>Taste JavaScript</label>
-                <button className="destroy"></button>
-              </div>
-              <input className="edit" defaultValue="Create a TodoMVC template" />
-            </li>
-            <li>
-              <div className="view">
-                <input className="toggle" type="checkbox" />
-                <label>Buy a unicorn</label>
-                <button className="destroy"></button>
-              </div>
-              <input className="edit" defaultValue="Rule the web" />
-            </li>
-          </ul>
-        </section>
-        <footer className="footer">
-          <span className="todo-count">
-            <strong>0</strong> item left
-          </span>
-          <ul className="filters">
-            <li>
-              <a className="selected" href="#/">
-                All
-              </a>
-            </li>
-            <li>
-              <a href="#/active">Active</a>
-            </li>
-            <li>
-              <a href="#/completed">Completed</a>
-            </li>
-          </ul>
-          <button className="clear-completed">Clear completed</button>
-        </footer>
+        <Header />
+        {todoLength > 0 && <Content />}
+        {todoLength > 0 && <Footer />}
       </section>
       <footer className="info">
-        <p>Double-click to edit a todo</p>
+        <p>Triple-click to edit a todo</p>
         <p>
-          Created by <a href="http://todomvc.com">Haruki Nguyen</a>
+          Created by{" "}
+          <a href="http://todomvc.com">
+            <b>Haruki Nguyen</b>
+          </a>
         </p>
         <p>
           Part of <a href="http://todomvc.com">TodoMVC</a>
