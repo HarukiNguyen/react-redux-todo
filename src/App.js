a3eb46909cd1e5effb97937fed97307b638a9798
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2022-12-19 22:07:54 +0700

    feat: update author link

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { useSelector } from "react-redux";
-import { selectAllTodos } from "./features/TodosList/todosSlice";
-import Content from "./layouts/Content";
-import Footer from "./layouts/Footer";
-import Header from "./layouts/Header";
+import { useSelector } from 'react-redux';
+import { selectAllTodos } from './features/TodosList/todosSlice';
+import Content from './layouts/Content';
+import Footer from './layouts/Footer';
+import Header from './layouts/Header';
 
 function App() {
   const todos = useSelector(selectAllTodos);
@@ -10,21 +10,21 @@ function App() {
 
   return (
     <>
-      <section className="todoapp">
+      <section className='todoapp'>
         <Header />
         {todoLength > 0 && <Content />}
         {todoLength > 0 && <Footer />}
       </section>
-      <footer className="info">
+      <footer className='info'>
         <p>Double-click to edit a todo</p>
         <p>
-          Created by{" "}
-          <a href="http://todomvc.com">
+          Created by{' '}
+          <a href='https://www.linkedin.com/in/haruki-nguyen-799b0a255/'>
             <b>Haruki Nguyen</b>
           </a>
         </p>
         <p>
-          Part of <a href="http://todomvc.com">TodoMVC</a>
+          Part of <a href='http://todomvc.com'>TodoMVC</a>
         </p>
       </footer>
     </>
